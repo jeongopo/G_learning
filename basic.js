c1af108598b1c688bdb7bbf3c40f9e7d7d86900d
Hyun2and <52280545+jeongopo@users.noreply.github.com>
2022-04-07 22:40:42 +0900

    추가

---
+++ basic.js
@@ -0,0 +1,26 @@
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload() {
+    this.load.image('circle', "assets/circle1.png");
+}
+
+var platforms;
+
+function create() {
+    this.add.image(400, 300, 'circle');
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'circle');
+}
+
+function update() {}
