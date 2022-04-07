541289ce320c8925e5147ca155e329f13fe41cd7
Hyun2and <52280545+jeongopo@users.noreply.github.com>
2022-04-08 01:16:04 +0900

    1일차 셋팅
    움직이는게 안된다

--- basic.js
+++ basic.js
@@ -1,29 +1,43 @@
 var config = {
     type: Phaser.AUTO,
-    width: 800,
+    width: 1920,
     height: 600,
     scene: {
         preload: preload,
         create: create,
         update: update
-    }
+    },
+  physics: {
+            default: 'arcade',
+            arcade: {
+                debug: false
+            }
+        },
 };
 
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image('circle', "assets/circle1.png");
+    this.load.setBaseURL('http://jeongopo.dothome.co.kr');
+    this.load.image('circle', "assets/smallcircle2.png");
 }
 
 var platforms;
-var circle;
+var circle, textEval,eval,cursors;
+var key;
+
 function create() {
-    circle=this.add.image(0, 0, 'circle');
+    circle=this.add.image(300, 400, 'circle');
+  cursors = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    textEval=this.add.text(16, 16, '판정', { font:"bold 24px sans-serif", fill: '#FFF' });
+  eval="MISS";
 }
 
 function update() {
-    var cursors = this.input.keyboard.createCursorKeys();
-    if(cursors.w.isDown){
-        circle.setVelocityX(-160);
+  circle.setVelocityX(3);
+    if(cursors.isDown){
+        console.log("W를 누름");
+	eval="Perfect";
     }
+  textEval.setText("판정 : "+eval);
 }
